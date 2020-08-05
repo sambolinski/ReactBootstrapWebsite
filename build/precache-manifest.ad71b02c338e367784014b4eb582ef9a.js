5146b4119def129ac7536cd9e6ee1e7227d36247
sambolinski <sambolinski@gmail.com>
2020-08-05 16:01:42 +0100

    added build

---
+++ build/precache-manifest.ad71b02c338e367784014b4eb582ef9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c339fd99abd23e775d7c844a3e3e0c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2441eebb253d7f83bd23",
+    "url": "/static/css/main.02052e39.chunk.css"
+  },
+  {
+    "revision": "23eac4f691ccabe5a8fe",
+    "url": "/static/js/2.cb885632.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cb885632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2441eebb253d7f83bd23",
+    "url": "/static/js/main.e4156af6.chunk.js"
+  },
+  {
+    "revision": "9f3b9fe826fd21aa5eea",
+    "url": "/static/js/runtime-main.f7850296.js"
+  },
+  {
+    "revision": "dfa66a10f7f180fe03a7e94fef43765d",
+    "url": "/static/media/Mandelbrot.dfa66a10.png"
+  }
+]);
